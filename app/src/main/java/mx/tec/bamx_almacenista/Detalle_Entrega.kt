fdc780d570e7333d12b2fc5a4ed7c62be4d6eb08
Vero <a01275996@itesm.mx>
2021-10-13 16:59:25 -0500

    volley p1

--- app/src/main/java/mx/tec/bamx_almacenista/Detalle_Entrega.kt
+++ app/src/main/java/mx/tec/bamx_almacenista/Detalle_Entrega.kt
@@ -2,53 +2,120 @@ package mx.tec.bamx_almacenista
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.detalle_entrega.*
 import kotlinx.android.synthetic.main.donativo_entregado.*
 import kotlinx.android.synthetic.main.layout_entrega.*
 import kotlinx.android.synthetic.main.toolbar.*
+import mx.tec.bamx_almacenista.ListView.CantidadEntrega
 import mx.tec.bamx_almacenista.ListView.Model_Entrega
+import org.json.JSONObject
 
 class Detalle_Entrega : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         var index: Int = 0
-        var estatus: Boolean = true
+        var total: Int = 0
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detalle_entrega)
 
-        val operario = intent.getStringExtra("operario")
+        var queue = Volley.newRequestQueue(this@Detalle_Entrega)
+        val url = "http://192.168.3.36:5000/warehouseman/detalle-entrega/12"
+        val datos = mutableListOf<CantidadEntrega>() // mutableListOf para lista dinámica
+        var lista = ArrayList<String>()
 
-        val datos = listOf(
-            Model_Entrega("Fernando", R.drawable.camion, "W01", "TLAHUAPAN", 50, 50, 50, 50),
-            Model_Entrega("Ricardo", R.drawable.camion, "W02",  "REFRIGERADOS",70, 30, 10, 50),
-            Model_Entrega("Lucio", R.drawable.camion, "W03",  "ZAPATA",90, 40, 50, 90),
-            Model_Entrega("Cristián", R.drawable.camion, "W04",  "TLAHUAPAN",7, 50, 6, 30),
-            Model_Entrega("Ángel", R.drawable.camion, "W05",  "REFRIGERADOS",8, 10, 6, 50),
-            Model_Entrega("Alejandro", R.drawable.camion, "W06",  "TLAHUAPAN",0, 50, 9, 50),
-        )
-        println("OPERARIO "+ operario)
-        while(estatus) {
-            if(operario.equals(datos[index].operario)) {
-                txtAbarroteCant.text = datos[index].cantAbarrote.toString()
-                txtFrutaVCant.text = datos[index].cantFruta.toString()
-                txtPanCant.text = datos[index].cantPan.toString()
-                txtNoComerCant.text = datos[index].cantNoComer.toString()
-                //exitProcess(0)
-                estatus = false
-            } else {
-                index += 1
+        val id = intent.getIntExtra("id", 0)
+        //val id: Int = 12
+        println("id "+ id)
+
+        val listener = Response.Listener<JSONObject> { response ->
+            Log.e("RESPONSE", response.toString())
+
+            val array = response.getJSONArray("data")
+            for (i in 0 until array.length()) {
+                datos.add(
+                    CantidadEntrega(array.getJSONObject(i).getInt("idDonativo"),
+                        array.getJSONObject(i).getString("folio"),
+                        array.getJSONObject(i).getString("fecha"),
+                        array.getJSONObject(i).getString("bodega"),
+                        array.getJSONObject(i).getString("nombre"),
+                        array.getJSONObject(i).getString("apellidoPaterno"),
+                        array.getJSONObject(i).getString("apellidoMaterno"),
+
+                        array.getJSONObject(i).getInt("kg_frutas_verduras"),
+                        array.getJSONObject(i).getInt("kg_pan"),
+                        array.getJSONObject(i).getInt("kg_abarrotes"),
+                        array.getJSONObject(i).getInt("kg_no_comestibles")
+                    ))
+                if (id == datos[i].id) {
+                    txtAbarroteCant.text = datos[i].cantAbarrote.toString()
+                    txtFrutaVCant.text = datos[i].cantFruta.toString()
+                    txtPanCant.text = datos[i].cantPan.toString()
+                    txtNoComerCant.text = datos[i].cantNoComer.toString()
+
+                    total = datos[i].cantAbarrote + datos[i].cantFruta +datos[i].cantPan + datos[i].cantNoComer
+                    //println("TOTAL "+ total)
+                    txtTotalCant.text = total.toString()
+
+                    index = i
+
+                    lista.add(datos[i].id.toString())
+
+                    lista.add(datos[i].folio)
+                    lista.add(datos[i].fecha)
+                    lista.add(datos[i].almacen)
+                    lista.add(datos[i].nombre)
+                    lista.add(datos[i].apPaterno)
+                    lista.add(datos[i].apMaterno)
+
+                    lista.add(datos[i].cantAbarrote.toString())
+                    lista.add(datos[i].cantFruta.toString())
+                    lista.add(datos[i].cantPan.toString())
+                    lista.add(datos[i].cantNoComer.toString())
+
+                }
             }
         }
-        println("INDEX "+ index)
+       /* println("index "+ index)
+        while(estatus) {
+            if (id != null) {
+                if (id == datos[index].id) {
+                    txtAbarroteCant.text = datos[index].cantAbarrote.toString()
+                    txtFrutaVCant.text = datos[index].cantFruta.toString()
+                    txtPanCant.text = datos[index].cantPan.toString()
+                    txtNoComerCant.text = datos[index].cantNoComer.toString()
+                    //exitProcess(0)
+                    estatus = false
+                } else if(datos.length <= index){
+                    index += 1
+                } else {
+                    estatus = false
+                }
+            }
+        }*/
 
-        val total: Int
-        total = datos[index].cantAbarrote + datos[index].cantFruta +datos[index].cantPan + datos[index].cantNoComer
-        println("TOTAL "+ total)
-        txtTotalCant.text = total.toString()
+            val error = Response.ErrorListener { error ->
+                Log.e("ERROR", error.message!!)
+            }
+            val peticion = JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                listener,
+                error
+            )
+            queue.add(peticion)
+
 
         btnContinuar.setOnClickListener {
             val intent = Intent(this@Detalle_Entrega, DonativoEntregado::class.java)
-            intent.putExtra("operario", datos[index].operario)
+            intent.putExtra("id", datos[index].id)
+            intent.putStringArrayListExtra("lista", lista)
             // Quita login del stack y deja al MAin como principal
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
--- app/src/main/java/mx/tec/bamx_almacenista/ProximasEntregas.kt
+++ app/src/main/java/mx/tec/bamx_almacenista/ProximasEntregas.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
-import androidx.cardview.widget.CardView
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.layout_entrega.*
 import kotlinx.android.synthetic.main.proximas_entregas.*
@@ -13,42 +15,66 @@ import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.android.synthetic.main.toolbar.icon_salir
 import kotlinx.android.synthetic.main.toolbarsinflecha.*
 import mx.tec.bamx_almacenista.ListView.Entregas_Adapter
+import com.android.volley.toolbox.Volley
 import mx.tec.bamx_almacenista.ListView.Model_Entrega
+import org.json.JSONObject
 
 class ProximasEntregas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.proximas_entregas)
 
-        val lstElemnetos = findViewById<ListView>(R.id.lstEntregas)
+        var queue = Volley.newRequestQueue(this@ProximasEntregas)
+        val url = "http://192.168.3.36:5000/warehouseman/datos-entrega"
+        val datos = mutableListOf<Model_Entrega>() // mutableListOf para lista dinámica
 
-        val datos = listOf(
-            Model_Entrega("Fernando", R.drawable.camion, "W01", "TLAHUAPAN", 50, 50, 50, 50),
-            Model_Entrega("Ricardo", R.drawable.camion, "W02",  "REFRIGERADOS",70, 30, 10, 50),
-            Model_Entrega("Lucio", R.drawable.camion, "W03",  "ZAPATA",90, 40, 50, 90),
-            Model_Entrega("Cristián", R.drawable.camion, "W04",  "TLAHUAPAN",7, 50, 6, 30),
-            Model_Entrega("Ángel", R.drawable.camion, "W05",  "REFRIGERADOS",8, 10, 6, 50),
-            Model_Entrega("Alejandro", R.drawable.camion, "W06",  "TLAHUAPAN",0, 50, 9, 50),
-        )
+        val listener = Response.Listener<JSONObject> { response ->
+            Log.e("RESPONSE", response.toString())
+            val array = response.getJSONArray("data")
+            for (i in 0 until array.length()) {
+                datos.add(
+                    Model_Entrega(array.getJSONObject(i).getInt("id"),
+                        array.getJSONObject(i).getString("nombre"),
+                        array.getJSONObject(i).getString("apellidoMaterno"),
+                        array.getJSONObject(i).getString("apellidoPaterno"),
+                        R.drawable.camion,
+                        "\n" + array.getJSONObject(i).getInt("folio")
 
-        val adaptador = Entregas_Adapter(
-            this@ProximasEntregas,
-            R.layout.layout_entrega,
-            datos
-        )
-        lstElemnetos.adapter = adaptador
+                ))
+            }
 
-        lstElemnetos.setOnItemClickListener { parent, view, position, id ->
-            val intent = Intent(this@ProximasEntregas, Detalle_Entrega::class.java)
-            intent.putExtra("operario", datos[position].operario)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
+            val adaptador = Entregas_Adapter(
+                this@ProximasEntregas,
+                R.layout.layout_entrega,
+                datos
+            )
+            lstEntregas.adapter = adaptador
+
         }
 
-        icon_salir.setOnClickListener {
-            logout()
-        }
+            val error = Response.ErrorListener { error ->
+                Log.e("ERROR", error.message!!)
+            }
+            val peticion = JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                listener,
+                error
+            )
+            queue.add(peticion)
 
+
+        lstEntregas.setOnItemClickListener { parent, view, position, id ->
+                val intent = Intent(this@ProximasEntregas, Detalle_Entrega::class.java)
+                intent.putExtra("id", datos[position].id)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            }
+
+            icon_salir.setOnClickListener {
+                logout()
+            }
     }
 
     fun logout() {
